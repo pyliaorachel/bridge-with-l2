7d5d7052de6c90c7d0c5194a8f61a29b3294718b
Peiyu Liao <pyliao@stanford.edu>
2019-03-03 14:14:19 -0800

    Add utils

---
+++ project/src/utils/utils.py
@@ -0,0 +1,15 @@
+import re
+
+
+def num_human_format(num):
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '{}{}'.format(round(num), ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
+
+def is_chinese(text):
+    return len(re.findall('[\u4e00-\u9fff]+', text)) > 0
+
+def always_true(x):
+    return True
